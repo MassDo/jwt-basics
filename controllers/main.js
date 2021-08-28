72ea9ee070a6ec76b617d9398f1f8051c0419b1b
massdo <dorian_massoulier@hotmail.com>
2021-08-28 19:45:06 +0200

    JWT creation in login controller check if it exists in headers for dashboard controller

--- controllers/main.js
+++ controllers/main.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
 const CustomAPIError = require('../errors/custom-error')
 
 const login = async (req, res)=>{
@@ -5,9 +7,18 @@ const login = async (req, res)=>{
     if (!username || !password){
         throw new CustomAPIError('You need to provide username and password ! :(', 400)
     }
-    res.send('Fake Login/Register/Signup Route')
+    // fake id for token
+    const id = Date.now()
+    const token  = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn:'30d'})
+    res.status(200).json({msg:`Bonjour ${username} voici vos données secrètes`, token:token})
 }
 const dashboard = async (req, res)=>{
+    const authHeader = req.headers.authorization
+    if (!authHeader || !authHeader.startsWith('Bearer ')){
+        throw new CustomAPIError('No JWT given', 400)
+    }
+    const token = authHeader.split(' ')[1]
+    console.log(token);
     const luckyNumber = Math.floor(Math.random()*100)  
     res.status(200).json({msg:`Salut mon ami !`, secret:`Voici tes données apres authorisation: ${luckyNumber}`})
 }
